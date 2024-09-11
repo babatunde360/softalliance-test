294e052b794fc92a9d164cd4dc32637a827f4b58
Babatunde <babatundeola66@gmail.com>
2024-09-11 12:22:30 +0100

    fix: Update to initial account setup

--- employee-mgt/src/main/java/com/babatunde/employee/config/AdminAccountSetupLoader.java
+++ employee-mgt/src/main/java/com/babatunde/employee/config/AdminAccountSetupLoader.java
@@ -7,16 +7,20 @@ import com.babatunde.employee.model.department.response.DepartmentDTO;
 import com.babatunde.employee.model.employee.request.EmployeeCreationJSON;
 import com.babatunde.employee.service.department.DepartmentService;
 import com.babatunde.employee.service.employee.EmployeeService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class AdminAccountSetupLoader implements CommandLineRunner {
 
+    public static final String ADMIN = "Admin";
     private final EmployeeService userService;
     private final DepartmentService departmentService;
 
@@ -25,20 +29,29 @@ public class AdminAccountSetupLoader implements CommandLineRunner {
 
         try {
             if (!userService.findByEmail("admin@company.com")) {
+                Long departmentId;
+                Optional<DepartmentDTO> optionalDepartment = departmentService.findByName(ADMIN);
+                if (optionalDepartment.isEmpty()) {
+                    DepartmentCreationJSON.DepartmentCreationJSONBuilder department =
+                            DepartmentCreationJSON.builder()
+                                    .name(ADMIN);
 
-                DepartmentCreationJSON.DepartmentCreationJSONBuilder department =
-                        DepartmentCreationJSON.builder()
-                                .name("Admin");
+                    DepartmentDTO createdDepartment = departmentService.create(department.build());
 
-                DepartmentDTO createdDepartment = departmentService.create(department.build());
+                    departmentId = createdDepartment.getId();
+                } else {
+
+                    departmentId = optionalDepartment.get().getId();
+                }
+
 
                 EmployeeCreationJSON.EmployeeCreationJSONBuilder admin = EmployeeCreationJSON.builder();
 
-                admin.firstName("Admin");
+                admin.firstName(ADMIN);
                 admin.lastName("User");
                 admin.email("admin@company.com");
                 admin.password("Testing@123");
-                admin.departmentId(createdDepartment.getId());
+                admin.departmentId(departmentId);
                 admin.role(AuthRole.ADMIN);
 
                 userService.create(admin.build());
